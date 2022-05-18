f63f482624f5c2b62548e08ce4b17f09067d03ff
Kyle Ryu <kyleryu6698@gmail.com>
2022-05-18 21:14:18 +0900

    state test

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  var hello = 'blog test';
+  let hello = 'blog test';
+  let [글제목, b] = useState(['미금역 삼겹살', '짐싸 어플', '세탁기 청소']);
 
   return (
     <div className="App">
       <div className="black_nav">
-        <h1>Blog</h1>
+        <h1>ReactBlog</h1>
       </div>
       <h3 style={{color:'blue'}}>{hello}</h3>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>5월 18일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>5월 18일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>5월 18일 발행</p>
+      </div>
     </div>
   );
 }
